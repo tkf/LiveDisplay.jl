8726dd2351f3326103bf77ff57dac655b08b7b17
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-04-27 07:40:49 +0000

    build based on a47bcbc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#LiveDisplay.jl-1","page":"Home","title":"LiveDisplay.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [LiveDisplay]","category":"page"},{"location":"#LiveDisplay.@livedocs","page":"Home","title":"LiveDisplay.@livedocs","text":"@livedocs THING [display=FUNCTION]\n\nDisplay THING using FUNCTION (default to display) where THING can be:\n\nJulia object with docstring.\nPath to markdown file.\n\nExamples\n\njulia> @livedocs f\n\njulia> using ElectronDisplay\n\njulia> @livedocs \"README.md\" display=electrondisplay\n\n\n\n\n\n","category":"macro"}]
+}
