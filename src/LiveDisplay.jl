a47bcbc2e3794265f7b3e4d21b974be41c32524f
Takafumi Arakaki <aka.tkf@gmail.com>
2019-04-27 00:33:19 -0700

    Support displaying markdown file

--- src/LiveDisplay.jl
+++ src/LiveDisplay.jl
@@ -3,22 +3,45 @@ module LiveDisplay
 export @livedocs
 
 using Base.Docs: doc
-
+using Markdown
 using MacroTools
 using Revise
+
+watchables(thing) = ([], [parentmodule(thing)])
+asdoc(thing) = doc(thing)
+
+watchables(path::AbstractString) = ([path], [])
+asdoc(path::AbstractString) = Markdown.parse_file(path)
 
 calldisplay(::Nothing, x) = display(x)
 calldisplay(f, x) = f(x)
 
 function livedocs(thing; display=nothing)
-    calldisplay(display, doc(thing))
-    entr([], [parentmodule(thing)]) do
-        calldisplay(display, doc(thing))
+    calldisplay(display, asdoc(thing))
+    entr(watchables(thing)...) do
+        calldisplay(display, asdoc(thing))
     end
 end
 
 iskwarglike(expr, name) = isexpr(expr, :(=)) && expr.args[1] == name
 
+"""
+    @livedocs THING [display=FUNCTION]
+
+Display THING using FUNCTION (default to `display`) where THING can be:
+
+* Julia object with docstring.
+* Path to markdown file.
+
+# Examples
+```julia-repl
+julia> @livedocs f
+
+julia> using ElectronDisplay
+
+julia> @livedocs "README.md" display=electrondisplay
+```
+"""
 macro livedocs(expr, option=nothing)
     if isexpr(expr, :macrocall)
         args = filter(x -> x isa Expr, expr.args[2:end])
